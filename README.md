a80953ef0b46579df8558cc6f647dea158817e7b
pabratte <pabratte@gmail.com>
2016-08-15 21:51:24 -0300

    Agregado tp0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pvj2-fichunl-code
+# pvj2-fichunl-code 
+
+Código de ejemplos utilizados en la materia **Programación de Videojuegos II** de la **FICH-UNL**. Ver [pabab.github.io/pvj2-fichunl](http://pabab.github.io/pvj2-fichunl)
---
+++ tp0/source/PlayState.hx
@@ -0,0 +1,32 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+		var name: String = "pabab";
+		m_text = new FlxText(0, 100, 640);
+		m_text.text = "Hola HaxeFlixel!!!\nMi nombre es\n" + name;
+		m_text.alignment = "center";
+		m_text.size = 60;
+		add(m_text);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		m_text.color = FlxG.random.color();
+		m_text.angle = FlxG.random.float(-5, 5);
+		m_text.scale.x = m_text.scale.y = FlxG.random.float(0.7, 1);
+	}
+
+	private var m_text: FlxText;
+}
