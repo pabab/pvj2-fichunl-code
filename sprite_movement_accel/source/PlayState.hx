0c39138bb4b06dee205ef376080f1b6147cc6186
Pablo Abratte <pabratte@gmail.com>
2016-08-22 10:10:17 -0300

    Corregido sprite_movement_accel, agregados .zip y .swf para ejemplos de movimiento de sprites

--- sprite_movement_accel/source/PlayState.hx
+++ sprite_movement_accel/source/PlayState.hx
@@ -1,13 +1,34 @@
 import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
 
 class PlayState extends FlxState
 {
-	private var character: Character;
-
 	override public function create():Void
 	{
 		super.create();
-		character = new Character(100, 100);
-		add(character);
+		sprite = new FlxSprite(100, 100, "assets/images/ImpGuy_0.jpg");
+		sprite.drag.x = 200;
+		sprite.maxVelocity.x = 200;
+		add(sprite);
 	}
+
+	override public function update(elapsed):Void
+	{
+		super.update(elapsed);
+		if(FlxG.keys.pressed.A)
+		{
+			sprite.acceleration.x = -200;
+		}
+		else if(FlxG.keys.pressed.D)
+		{
+			sprite.acceleration.x = 200;
+		}
+		else
+		{
+			sprite.acceleration.x = 0;
+		}
+	}
+
+	private var sprite: FlxSprite;
 }
