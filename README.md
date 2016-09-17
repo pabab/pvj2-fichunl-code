c29b7021a7460a64bea975c53c45e087a064831e
Pablo Abratte <pabratte@gmail.com>
2016-09-16 23:32:17 -0300

    Agregado ejemplo group_collisions Agregados créditos a Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pvj2-fichunl-code 
 
 Código de ejemplos utilizados en la materia **Programación de Videojuegos II** de la **FICH-UNL**. Ver [pabab.github.io/pvj2-fichunl](http://pabab.github.io/pvj2-fichunl)
+
+
+## Assets used
+* *Imp Demon sprite*. License: [CC0](http://creativecommons.org/publicdomain/zero/1.0/). Author: [yoyo007008](http://opengameart.org/users/yoyo007008). Published on [OpenGameArt.org](http://opengameart.org/content/imp-demon-sprite) 
+* Sprites and tiles from *Evoland*. Author: Nicolas Cannasse. Published on [Github](https://github.com/openfl/EvolandClassic)
+* *16x16 Small and Medium Coin Animation*. License: [CC0](http://creativecommons.org/publicdomain/zero/1.0/). Author: [patvanmackelberg](http://opengameart.org/users/patvanmackelberg). Published on [OpenGameArt.org](http://opengameart.org/content/16x16-small-and-medium-coin-animation) 
---
+++ group_collisions/source/PlayState.hx
@@ -0,0 +1,76 @@
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxObject;
+import flixel.group.FlxGroup;
+import flixel.tile.FlxBaseTilemap;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledTileLayer;
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+		bgColor = 0xff849684;
+		level = new FlxTilemap();
+		var tiledMap = new TiledMap("assets/data/levels/level01.tmx");
+		var layer = cast(tiledMap.getLayer("solid"), TiledTileLayer);
+		level.loadMapFromArray(layer.tileArray, layer.width, layer.height, "assets/images/tiles_alpha.png", 16, 16, FlxTilemapAutoTiling.OFF, 1);
+		char = new Character(64, 64);
+		enemies = new FlxTypedGroup<Enemy>();
+		coins = new FlxTypedGroup<Coin>();
+		characterCollideables = new FlxGroup();
+
+		coins.add(new Coin(160, 64));
+		coins.add(new Coin(160, 80));
+		coins.add(new Coin(160, 96));
+		coins.add(new Coin(64, 144));
+		coins.add(new Coin(80, 144));
+		coins.add(new Coin(96, 144));
+		coins.add(new Coin(256, 80));
+		coins.add(new Coin(256, 96));
+		coins.add(new Coin(256, 112));
+		coins.add(new Coin(256, 128));
+		coins.add(new Coin(256, 144));
+		coins.add(new Coin(256, 160));
+		enemies.add(new Enemy(256, 176));
+		enemies.add(new Enemy(240, 48));
+		enemies.add(new Enemy(160, 112));
+
+		add(level);
+		add(coins);
+		add(enemies);
+		add(char);
+		characterCollideables.add(enemies);
+		characterCollideables.add(level);
+		characterCollideables.add(coins);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		FlxG.overlap(characterCollideables, char, onCollisionWithChar);
+		FlxG.collide(level, enemies);
+	}
+
+	private function onCollisionWithChar(theObject: FlxObject, theChar: Character)
+	{
+		if(theObject == level){
+			FlxObject.separate(theObject, theChar);
+		}
+		else if(Type.getClass(theObject) == Enemy)
+		{
+			theChar.hit();
+		}
+		else if(Type.getClass(theObject) == Coin){
+			theObject.kill();
+		}
+	}
+
+	private var char: Character;
+	private var level: FlxTilemap;
+	private var enemies: FlxTypedGroup<Enemy>;
+	private var coins: FlxTypedGroup<Coin>;
+	private var characterCollideables: FlxGroup;
+}
